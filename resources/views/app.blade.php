b4b3a22ee269f5fd33b5cf40c05a09c2f94e6864
ANDRES-MARACALDO-11 <santyarias328@gmail.com>
2025-04-10 08:12:50 -0500

    add

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/print.css') }}" media="print">
+    <link rel="stylesheet" href="{{ asset('css/print.css') }}" media="screen">
+    
